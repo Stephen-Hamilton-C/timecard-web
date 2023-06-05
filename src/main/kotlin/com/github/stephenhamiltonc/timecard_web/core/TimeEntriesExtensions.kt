4ad4f6336a69048eb82444238d42063a1daae4e6
Stephen-Hamilton-C <60950821+Stephen-Hamilton-C@users.noreply.github.com>
2023-06-05 08:29:49 -0400

    Created TimeEntriesExtensions

---
+++ src/main/kotlin/com/github/stephenhamiltonc/timecard_web/core/TimeEntriesExtensions.kt
@@ -0,0 +1,20 @@
+package com.github.stephenhamiltonc.timecard_web.core
+
+import com.github.stephenhamiltonc.timecard.TimeEntries
+import kotlin.browser.localStorage
+
+private const val entriesStorageKey = "timeentries"
+
+fun TimeEntries.saveToStorage() {
+    val entriesData = this.toString()
+    localStorage[entriesStorageKey] = entriesData
+}
+
+fun TimeEntries.Companion.loadFromStorage() {
+    val entriesData = localStorage[entriesStorageKey]
+    return if(entriesData != null) {
+        TimeEntries.fromString(entriesData)
+    } else {
+        TimeEntries()
+    }
+}
