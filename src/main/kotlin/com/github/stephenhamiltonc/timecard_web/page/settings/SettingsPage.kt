19c432483542c1b500b3d47768d97802e20410f8
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-06-07 22:03:02 -0400

    Created simple Page components

---
+++ src/main/kotlin/com/github/stephenhamiltonc/timecard_web/page/settings/SettingsPage.kt
@@ -0,0 +1,10 @@
+package com.github.stephenhamiltonc.timecard_web.page.settings
+
+import io.kvision.html.span
+import io.kvision.panel.SimplePanel
+
+class SettingsPage : SimplePanel() {
+    init {
+        span("Settings page")
+    }
+}
