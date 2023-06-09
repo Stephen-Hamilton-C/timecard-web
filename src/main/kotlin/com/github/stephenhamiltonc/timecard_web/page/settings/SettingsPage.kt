7d5782c12d460730074979b15c7eda8ca3de5125
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-06-08 21:49:13 -0400

    Added message to Settings page

--- src/main/kotlin/com/github/stephenhamiltonc/timecard_web/page/settings/SettingsPage.kt
+++ src/main/kotlin/com/github/stephenhamiltonc/timecard_web/page/settings/SettingsPage.kt
@@ -1,10 +1,15 @@
 package com.github.stephenhamiltonc.timecard_web.page.settings
 
-import io.kvision.html.span
+import com.github.stephenhamiltonc.timecard_web.card
 import io.kvision.panel.SimplePanel
+import io.kvision.utils.px
 
 class SettingsPage : SimplePanel() {
     init {
-        span("Settings page")
+        marginTop = 12.px
+
+        card(title = "Settings is not yet implemented") {
+            +"It will be soon!"
+        }
     }
 }
