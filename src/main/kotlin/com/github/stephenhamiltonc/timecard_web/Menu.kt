c2c3a074f950b70b447375a2171803f8f224e5ef
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-06-09 23:09:14 -0400

    Created the credits page

--- src/main/kotlin/com/github/stephenhamiltonc/timecard_web/Menu.kt
+++ src/main/kotlin/com/github/stephenhamiltonc/timecard_web/Menu.kt
@@ -1,5 +1,6 @@
 package com.github.stephenhamiltonc.timecard_web
 
+import com.github.stephenhamiltonc.timecard_web.page.credits.CreditsPage
 import com.github.stephenhamiltonc.timecard_web.page.home.HomePage
 import com.github.stephenhamiltonc.timecard_web.page.settings.SettingsPage
 import io.kvision.core.Container
@@ -8,18 +9,23 @@ import io.kvision.panel.TabPosition
 import io.kvision.panel.VPanel
 import io.kvision.panel.tabPanel
 import io.kvision.utils.px
+import io.kvision.utils.vh
 
 class Menu : VPanel(className = "tc-menu") {
     init {
+        height = 100.vh
+
         tabPanel(
             tabPosition = TabPosition.LEFT,
             sideTabSize = SideTabSize.SIZE_1
         ) {
             marginLeft = 6.px
             marginRight = 6.px
+            height = 100.vh
 
             addTab("Home", HomePage(), route = "/")
             addTab("Settings", SettingsPage(), route = "/settings")
+            addTab("Credits", CreditsPage(), route = "/credits")
         }
     }
 }
