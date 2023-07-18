64188e1ed24b254a5a66ee0c2dea2baf6cdd9288
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-07-17 22:35:14 -0400

    Settings
    Moved download file code to StringExtensions

--- src/main/kotlin/com/github/stephenhamiltonc/timecard_web/core/StringExtensions.kt
+++ src/main/kotlin/com/github/stephenhamiltonc/timecard_web/core/StringExtensions.kt
@@ -1,7 +1,21 @@
+import kotlinx.browser.document
+import org.w3c.dom.HTMLAnchorElement
+import org.w3c.dom.url.URL
+import org.w3c.files.Blob
+import org.w3c.files.BlobPropertyBag
+
 fun String.pluralize(number: Number, plural: String = "${this}s"): String {
     return if(number != 1) {
         plural
     } else {
         this
     }
+}
+
+fun String.downloadAsFile(type: String, fileName: String) {
+    val blob = Blob(arrayOf(this), BlobPropertyBag(type))
+    val anchorElement = document.createElement("a") as HTMLAnchorElement
+    anchorElement.download = fileName
+    anchorElement.href = URL.createObjectURL(blob)
+    anchorElement.click()
 }
